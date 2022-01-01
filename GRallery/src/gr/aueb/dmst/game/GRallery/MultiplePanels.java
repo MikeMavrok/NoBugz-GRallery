33bcbf43d95a979807ab0115ba7de4fc9fd994ed
zosoe <93041088+zosoe@users.noreply.github.com>
2022-01-01 22:28:15 +0200

    Add files via upload
    το αρχείο της Γιούλης απλά με καποια arraylist δοκιμαστικά

--- GRallery/src/gr/aueb/dmst/game/GRallery/MultiplePanels.java
+++ GRallery/src/gr/aueb/dmst/game/GRallery/MultiplePanels.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Iterator;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -18,78 +19,250 @@ import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
 
 public class MultiplePanels implements ActionListener{
-	
+
+
+
 	JFrame frame = new JFrame("GRallery");
 	JButton[][] b = new JButton[10][4];
 	JButton NextQuestion = new JButton("Next Question");
-	String CorrectAnswers [] = new String[10];
+	ArrayList <String> CorrectAnswers  = new ArrayList<String>();
 	String[][] answers = new String[10][4];
-	String[] questions = new String[10];
+	ArrayList<String> questions = new ArrayList<String>();
 	ArrayList<ImageIcon> images = new ArrayList<ImageIcon>();
-	int question_number;
+	int question_number = 0;
+
 
 	public void ConstructImages() {
 		images.add(new ImageIcon(getClass().getResource("/faros.jpg")));
 		images.add(new ImageIcon(getClass().getResource("/delfoi.jpg")));
 		images.add(new ImageIcon(getClass().getResource("/oreli.jpg")));
 	}
-	
+
 	public void ConstructAnswers() {
-		for(int i = 0; i<questions.length ; i++) {
+		for(int i = 0; i<questions.size() ; i++) {
 			answers[i][0] = "answer1";
 			answers[i][1] = "answer2";
 			answers[i][2] = "answer3";
 			answers[i][3] = "answer4";
 		}
 	}
-	
+/* adding questions in the ArrayList named questions,the empty cells will be filled if this is running properly*/
 	public void ConstructQuestions() {
-		questions[0] = "Ο «Φάρος» εικάζεται πως αποτελεί μία απεικόνιση ενός πραγματικού φάρου σε κάποιο κυκλαδίτικο\n νησί με σκοπό σε συνδυασμό με άλλα έργα να αναδειχθεί το ελληνικό καλοκαίρι.Σε ποιον γνωστό\n Έλληνα ζωγράφο με καταγωγή από την Τήνο ανήκει ο συγκεκριμένος πίνακας;";
-		questions[1] = "Η σύνθεση του πίνακα διαμορφώνεται στη βάση της αντίθεσης φωτεινού και σκοτεινού και στο χωρισμό\n της επιφάνειας σε μεγάλες χρωματικές ενότητες με τα χρώματα να εναλλάσσονται από πορτοκαλί και\n κεραμιδί σε ανοιχτό γκρίζο, γαλάζιο και πράσινο. Τα έντονα και φωτεινά χρώματα που χρησιμοποιεί\n ο Κωνσταντίνος Μαλέας, τόσο στο συγκεκριμένο όσο και σε άλλα έργα του, και οι πλατιές πινελιές\n του αποτέλεσαν αφορμή για να χαρακτηριστούν τα έργα του από τους συντηρητικούς\n κριτικούς εκείνης της εποχής ως:\n ";
-		questions[2] = "Η ιστορία πίσω από τον συγκεκριμένο πίνακα όπως αφηγείται ο ίδιος ο ζωγράφος, Περικλής Βυζάντιος, είναι η εξής:\n“H μεγαλύτερή μου χαρά ήταν όταν η Oρελί μου έλεγε:«Allons nous coucher» και μ’ έπαιρνε από το χέρι και πηγαίναμε\nγια ύπνο στην κρεβατοκάμαρή μας. Άναβε ένα κερί σε ένα μπρούτζινο σαμντάνι επάνω στον κομό.Εγώ γρήγορα\nγρήγορα γδυνόμουνα και φόραγα τη μακριά πουκαμίσα κι έμπαινα στο κρεβάτι κι έκανα πως κοιμόμουνα, ενώ με το\nένα μάτι ανοιχτό παρακολουθούσα το γδύσιμο της Oρελί μπροστά στον καθρέφτη και τη θαύμαζα. H Oρελί ήτανε \nκαλοφτιαγμένη, ψηλή, με άφθονα καστανά μαλλιά. Βέβαιη πως εγώ κοιμάμαι, γδυνότανε σιγά σιγά, γυρίζοντάς μου\n την πλάτη. Τα διάφορα νταντελωτά μεσοφόρια, μεγάλα λεπτά εσώρουχα περασμένα με ροζ κορδέλες,της έπεφταν\n στα ωραία της πόδια, και γυμνή εντελώς σκούπιζε με μια κρέμα το πρόσωπό της σκυμμένη προς τον καθρέφτη. Μα\nεγώ δεν έβλεπα μονάχα τη ράχη της μέσα στον καθρέφτη διέκρινα και το σφιχτό και στέριο στήθος, που πολλές φορές\nτο κρατούσε με τα ωραία της χέρια και το καμάρωνε και η ίδια.Στο τέλος φορούσε μια μακριά νυχτικιά με κεντήματα \nστο στήθος και στις άκρες των μανικιών, πλησίαζε το κερί, το έσβηνε, κι ερχότανε στο μικτό κρεβάτι μου να με\n σκεπάσει καλά.Έσκυβε και με φιλούσε στο μέτωπο. Εγώ, φυσικά,έκανα πως κοιμόμουνα, και ούτε ανέπνεα, γιατί\n φοβόμουνα μήπως ακούσει την καρδιά μου που χτυπούσε δυνατά…” Ποιος πιστεύετε ότι είναι ο τίτλος του έργου;";
+		questions.add("What other famous painter do you know that the one depicted reminds you of?");
+		questions.add("He is one of the greatest British poets and remains popular even today.His characteristic work is the poem <<Don Juan>>. What's his name;");
+		questions.add("Which evangelical scene is depicted in the picture above?");
+		questions.add("The above painting by Nikolaos Othoneos refers to a specific time of year.Based on the colors he uses and the natural elements he presents, what do you think it is?");
+		questions.add("Looking carefully at the details of the painting,which Cycladic island do you think is depicted?");
+		questions.add("What is it called in painting and other arts, the apt depiction of a person,but also of the personality, emotions and psychic world of that person as perceived by the creator?");
+		questions.add("Which ancient temple is depicted in the painting?");
+		questions.add("This emblematic element refers to the occupation of the depicted.Based on this information, what do you think was Psarianos' profession?");
+		questions.add("What kind of event is represented in this painting?");
+		questions.add("Based on the clothes of the person depicted and the pipe he is holding,what would you say is his origin?");
+		questions.add("What is the name of the depiction of the marine theme in the visual arts?");
+		questions.add("Who wrote the poem <<The Destruction of the Fishermen>> from which Nikolaos Gyzis was inspired to create the Glory of the <<Fishermen>>?");
+		questions.add("Greek ___ is inspired by the manners and customs of the Greek people,as is the case with the corresponding literature of the time. It is no coincidencethat at the same time the sciences of Folklore and Linguistics were created,while at the same time the controversy over the value of the vernacular began.");
+		questions.add("Which celebration about this painting is described in the text?");
+		questions.add("A work by Dimitrios Galanis that was created around 1924.What do you think is its title?");
+		questions.add("What is the name of the custom depicted in this work?");
+		questions.add("What do you think the gentleman on the board is doing?");
+		questions.add("Also known as <<the Mountain of Centaurs>>. It is located very close to the city of Volos and uniquely combines the mountain with the sea. It consists of many small villagesand is famous for its unique architecture. What is the name of this place?");
+		/*questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();
+		questions.add();*/
 	}
-	
+
+/* adding the CorrectAnswers in the ArrayList named CorrectAnswers ,the empty cells will be filled if this is running properly*/
 	public void CorrectAnswers() {
-		CorrectAnswers[0] = answers[0][1];
-		CorrectAnswers[1] = answers[1][1];
-		CorrectAnswers[2] = answers[2][0];
+		CorrectAnswers.add("Frida Kahlo");
+		CorrectAnswers.add("Lord Byron");
+		CorrectAnswers.add("The Assumption of the Virgin");
+		CorrectAnswers.add("Spring");
+		CorrectAnswers.add("Santorini");
+		CorrectAnswers.add("Portrait");
+		CorrectAnswers.add("The Acropolis");
+		CorrectAnswers.add("Captain");
+		CorrectAnswers.add("Circus");
+		CorrectAnswers.add("East");
+		CorrectAnswers.add("Seascape");
+		CorrectAnswers.add("Dionysios Solomos");
+		CorrectAnswers.add("Ethography");
+		CorrectAnswers.add("Carnival");
+		CorrectAnswers.add("The hunter");
+		CorrectAnswers.add("Child engagements");
+		CorrectAnswers.add("He is looking at the painting");
+		CorrectAnswers.add("Pelion");
+		/*CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();
+		CorrectAnswers.add();*/
 	}
 
 
 	MultiplePanels(){
-		
+
 		ConstructQuestions();
     		ConstructImages();
     		ConstructAnswers();
     		CorrectAnswers();
-    
+
     		//create 3 different panels to place our labels (image-text-buttons)
-    		JPanel panel1 = new JPanel(); 
+    		JPanel panel1 = new JPanel();
     		JPanel panel2 = new JPanel();
     		JPanel panel3 = new JPanel();
-    	
+
     		//disable button NextQuestion so as to the user not be able to press it unless they first answer existed question
 		NextQuestion.setEnabled(false);
-		//randomly choose a question 
+		//randomly choose a question
 		Random rand = new Random();
     		question_number = rand.nextInt(3);
-    
+
 		panel1.setBackground(Color.white);
 		panel2.setBackground(Color.white);
 		panel3.setBackground(Color.white);
-		
+
 		//add image to panel1
 		JLabel imageLabel = new JLabel(images.get(question_number));
 		panel1.add(imageLabel);
-		
+
 		//add question to label3
 		JTextArea questionLabel = new JTextArea(10,65);
 		questionLabel.setEditable(false);
 		questionLabel.setSize(200, 200);
 		questionLabel.setFont(new Font(null,Font.LAYOUT_LEFT_TO_RIGHT,20));
-		questionLabel.setText(questions[question_number]);
+		questionLabel.setText(questions.get(question_number));
 		panel3.add(questionLabel,BorderLayout.BEFORE_FIRST_LINE);
-		
+
 		//add message that informs the player if their answer was correct or wrong to panel3
 		JLabel messageLabel = new JLabel();
     		messageLabel.setSize(100, 100);
@@ -97,10 +270,10 @@ public class MultiplePanels implements ActionListener{
 		messageLabel.setHorizontalTextPosition(JLabel.CENTER);
 		messageLabel.setVerticalTextPosition(JLabel.CENTER);
 		panel3.add(messageLabel,BorderLayout.AFTER_LINE_ENDS);
-		//add a scroll pane to panel3 
+		//add a scroll pane to panel3
 		JScrollPane scroll =new JScrollPane(questionLabel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		panel3.add(scroll);
-		
+
 		//create buttons which contain 4 suggested answers
 		b[question_number][0] = new JButton(answers[question_number][0]);
 		b[question_number][0].setBounds(10,40,160,40);
@@ -110,16 +283,16 @@ public class MultiplePanels implements ActionListener{
 		b[question_number][2].setBounds(10,40,160,40);
 		b[question_number][3] = new JButton(answers[question_number][3]);
 		b[question_number][3].setBounds(10,40,160,40);
-		
+
 		for(int answer_number=0; answer_number<4; answer_number++) {
 			//add buttons with suggested answers to panel2
 			b[question_number][answer_number].setFocusable(false);
-			panel2.add(b[question_number][answer_number]); 
+			panel2.add(b[question_number][answer_number]);
 			b[question_number][answer_number].setSize(new Dimension(500, 50));
 			b[question_number][answer_number].setLocation(100,100);
 			b[question_number][answer_number].setFont(new Font(null,Font.TRUETYPE_FONT,22));
 			b[question_number][answer_number].setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-	
+
 			b[question_number][answer_number].addActionListener(new ActionListener() {
 	        	public void actionPerformed(ActionEvent e) {
 	                if(e.getSource() == b[question_number][0] ) {
@@ -128,8 +301,8 @@ public class MultiplePanels implements ActionListener{
 	                	b[question_number][2].setEnabled(false);
 	                	b[question_number][3].setEnabled(false);
 	                	NextQuestion.setEnabled(true);
-	                	
-	                	if((b[question_number][0].getText()) == CorrectAnswers[question_number]) {
+
+	                	if((b[question_number][0].getText()) == CorrectAnswers.get(question_number)) {
 	                		//if they answer correct, pressed button turns green and message "CORRECT" shows up
 	                		messageLabel.setForeground(Color.green);
 					messageLabel.setText("CORRECT");
@@ -141,13 +314,13 @@ public class MultiplePanels implements ActionListener{
 	                		b[question_number][0].setBackground(Color.RED);
 	                	}
 	                }else if(e.getSource() == b[question_number][1] ) {
-	                	
+
 	                	b[question_number][0].setEnabled(false);
 	                	b[question_number][2].setEnabled(false);
 	                	b[question_number][3].setEnabled(false);
 	                	NextQuestion.setEnabled(true);
-	                	
-	                	if(b[question_number][1].getText() == CorrectAnswers[question_number]) {
+
+	                	if(b[question_number][1].getText() == CorrectAnswers.get(question_number)) {
 	                		messageLabel.setForeground(Color.green);
 	                		messageLabel.setText("CORRECT");
 	                		b[question_number][1].setBackground(Color.GREEN);
@@ -157,13 +330,13 @@ public class MultiplePanels implements ActionListener{
 	                		b[question_number][1].setBackground(Color.RED);
 	                	}
 	                }else if(e.getSource() == b[question_number][2] ) {
-	                	
+
 	                	b[question_number][0].setEnabled(false);
 	                	b[question_number][1].setEnabled(false);
 	                	b[question_number][3].setEnabled(false);
 	                	NextQuestion.setEnabled(true);
-	                	
-	                	if(b[question_number][2].getText() == CorrectAnswers[question_number]) {
+
+	                	if(b[question_number][2].getText() == CorrectAnswers.get(question_number)) {
 	                		messageLabel.setForeground(Color.green);
 	                		messageLabel.setText("CORRECT");
 	                		b[question_number][2].setBackground(Color.GREEN);
@@ -173,13 +346,13 @@ public class MultiplePanels implements ActionListener{
 	                		b[question_number][2].setBackground(Color.RED);
 	                	}
 	                }else if(e.getSource() == b[question_number][3] ) {
-	                	
+
 	                	b[question_number][0].setEnabled(false);
 	                	b[question_number][1].setEnabled(false);
 	                	b[question_number][2].setEnabled(false);
 	                	NextQuestion.setEnabled(true);
-	                	
-	                	if(b[question_number][3].getText() == CorrectAnswers[question_number]) {
+
+	                	if(b[question_number][3].getText() == CorrectAnswers.get(question_number)) {
 	                		messageLabel.setForeground(Color.green);
 	                		messageLabel.setText("CORRECT");
 	                		b[question_number][3].setBackground(Color.GREEN);
@@ -192,7 +365,7 @@ public class MultiplePanels implements ActionListener{
 	        	}
 	        	});
 			}
-			//add button NextQuestion to panel2 
+			//add button NextQuestion to panel2
 			NextQuestion.setBounds(0,500,100,100);
 			NextQuestion.setBackground(Color.BLACK);
 			NextQuestion.setForeground(Color.WHITE);
@@ -216,11 +389,11 @@ public class MultiplePanels implements ActionListener{
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
