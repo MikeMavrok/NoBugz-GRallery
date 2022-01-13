dc8ee658e6ba85f946e1dd3d8821c8f4f8609bc0
AikaAnast <93044710+AikaAnast@users.noreply.github.com>
2022-01-13 15:32:26 +0200

    Update Users.java

--- GRallery/src/gr/aueb/dmst/game/GRallery/Users.java
+++ GRallery/src/gr/aueb/dmst/game/GRallery/Users.java
@@ -5,8 +5,13 @@ public class Users {
 HashMap<String, String> logininfo = new HashMap<String, String>();
 	
 	Users(){
+		ogininfo.put("1","Κατερίνα");
+		logininfo.put("2","Ασημίνα");
+		logininfo.put("3","Ζωή");
+		logininfo.put("4","Γιώργος");
+		logininfo.put("5","Όλγα");
+		logininfo.put("6","Μιχάλης");
 		
-		logininfo.put("Test","Tester");
 	}
 	
 	public HashMap getLoginInfo() {
