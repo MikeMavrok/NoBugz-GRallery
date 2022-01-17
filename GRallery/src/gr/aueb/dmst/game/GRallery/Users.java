aed5ec8177736ccd8c2402897ff04566a90b7239
MikeMavrok <93074951+MikeMavrok@users.noreply.github.com>
2022-01-17 22:23:00 +0200

    Update Users.java

--- GRallery/src/gr/aueb/dmst/game/GRallery/Users.java
+++ GRallery/src/gr/aueb/dmst/game/GRallery/Users.java
@@ -5,7 +5,7 @@ public class Users {
 HashMap<String, String> logininfo = new HashMap<String, String>();
 	
 	Users(){
-		ogininfo.put("1","Κατερίνα");
+		logininfo.put("1","Κατερίνα");
 		logininfo.put("2","Ασημίνα");
 		logininfo.put("3","Ζωή");
 		logininfo.put("4","Γιώργος");
